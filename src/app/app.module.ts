f923a20f5f039a643880a26831c44f89e9a86a8b
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-08-24 09:50:35 +0300

    Auth form add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { GridListComponent } from './grid-list/grid-list.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridListComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
