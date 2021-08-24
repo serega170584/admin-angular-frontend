195b420ce22b157ad05abf1d6b51719198222032
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-08-24 10:13:29 +0300

    Auth form add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 import { GridListComponent } from './grid-list/grid-list.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { AuthFormComponent } from './auth-form/auth-form.component';
--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -7,14 +7,14 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./auth-form.component.css']
 })
 export class AuthFormComponent implements OnInit {
-  login = new FormControl('', [Validators.required]);
+  public login = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getErrorMessage() {
+  public getErrorMessage() {
     let error = '';
     if (this.login.hasError('required')) {
       error = 'You must enter a value';
