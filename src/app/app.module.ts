1f4c381abb1a5a4db9f0aad86984707127d35fd6
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-08-24 09:03:57 +0300

    Grid list component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { GridListComponent } from './grid-list/grid-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
