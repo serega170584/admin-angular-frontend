3e1b6435d18d12518237e99df36b7dbc2bc06f70
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-08-24 09:13:45 +0300

    Toolbar add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { GridListComponent } from './grid-list/grid-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridListComponent
+    GridListComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
