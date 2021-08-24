47226ead524d2f3342ffc8fae095ee3e5fbf521c
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-08-24 10:00:54 +0300

    Auth form add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { GridListComponent } from './grid-list/grid-list.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { AuthFormComponent } from './auth-form/auth-form.component';
@@ -22,7 +23,8 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatGridListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-form.component.css']
 })
 export class AuthFormComponent implements OnInit {
+  login = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getErrorMessage() {
+    let error = '';
+    if (this.login.hasError('required')) {
+      error = 'You must enter a value';
+    }
+    return error;
   }
 
 }
